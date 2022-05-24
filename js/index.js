abe7a832c5914080a5a715a2c78fca6b3dc0a858
Tom <1783703828@qq.com>
2022-05-24 14:29:52 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,77 +1,112 @@
 function open_list() {
     $(".vta_listPopupWindow").toggle();
 }
-function changeIcon(mainNode, clickNode) {
+function changeIcon(mainNode, clickNode, index) {
     if (mainNode) {
-        if (mainNode.attr("src").indexOf("down_black.png") >= 0) {
+        if (mainNode.attr("src").indexOf("down") >= 0) {
             mainNode.attr("src", "./img/up_blue.png");
             clickNode.addClass('list_item_active');
         } else {
-            mainNode.attr("src", "./img/down_black.png");
+            if (index == 0) {
+                mainNode.attr("src", "./img/down_black.png");
+            } else {
+                mainNode.attr("src", "./img/down.png");
+            }
             clickNode.removeClass('list_item_active');
         }
     }
 }
+
 // 弹出来点击事件
 $(function () {
     $('.list_item').click(function () {
-        // 	// 清除所有效果
-        // $(".list_child").hide()
-        // $(".list_item img").attr("src", "./img/down_black.png");
-        // $(".list_item").removeClass('list_item_active')
-        $(this).next('div').slideToggle();
-        // 更换图标添加类别
-        changeIcon($(this).children('img'), $(this));
+        var array = $(".list_item") // 主元素
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            if ($(this).attr('data-index') == index) {
+                $(this).next('div').slideToggle();
+                // 更换图标添加类别
+                changeIcon($(this).children('img'), $(this), 0);
+            } else {
+                $(element).next('div').hide();
+                $(element).find('img').attr("src", "./img/down_black.png");
+                $(element).removeClass('list_item_active')
+            }
+        }
+
     });
 });
 // 底部
 $(function () {
     $('.atBottomThe_item').click(function () {
         var array = $(".atBottomThe_item") // 主元素
-        var array_child = $(".atBottomThe_child")//子元素
         for (let index = 0; index < array.length; index++) {
             const element = array[index];
             if ($(this).attr('data-index') == index) {
-                if ($(this).attr('num') == 0) {
-                    $(array_child[index]).show()
-                    $(this).attr('num', 1)
-                    $(element).find('img').attr("src", "./img/up_blue.png");
-                    $(element).addClass('list_item_active')
-                } else {
-                    $(array_child[index]).hide()
-                    $(element).find('img').attr("src", "./img/down_black.png");
-                    $(element).removeClass('list_item_active')
-                    $(this).attr('num', 0)
-                }
-                // $(this).next('div').slideToggle();
-                // changeIcon($(this).children('img'), $(this));
+                // 更换图标添加类别
+                $(this).next('div').slideToggle();
+                changeIcon($(this).children('img'), $(this), 1);
             } else {
-                $(element).attr('num', 0)
-                $(array_child[index]).hide()
+                $(element).next('div').hide();
+                $(element).find('img').attr("src", "./img/down.png");
                 $(element).removeClass('list_item_active')
-                $(element).find('img').attr("src", "./img/down_black.png");
             }
         }
+    });
+});
+// 查看更多
+$(function () {
+    $('.toViewMore').click(function () {
+        $('#application_content').slideToggle();
+        if ($(this).attr('data-flag') == 0) {
+            $(this).find('text').text('向上收缩');
+            $(this).find('img').css("transform", "rotate(180deg)");
+            $(this).attr('data-flag', 1)
+        } else {
+            $(this).find('text').text('查看更多');
+            $(this).find('img').css("transform", "rotate(0deg)");
+            $(this).attr('data-flag', 0)
+        }
+
     });
 });
 //新闻 tab切换事件
 $(function () {
     $('#newsInformation_tabs a').click(function (e) {
         e.preventDefault();
-        $('#newsInformation_tabs li').removeClass("swich_btn_item_active");
-        $("#newsInformation_content div").removeClass("newsInformation_show");
-        $(this).parent().addClass("swich_btn_item_active");
-        $('#' + $(this).attr('title')).addClass('newsInformation_show');
+        var array = $("#newsInformation_tabs .swich_btn_item") // 主元素
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            if ($(this).parent().attr('data-index') == index) {
+                $('#' + $(this).attr('title')).show(1000);
+                $(this).parent().addClass("swich_btn_item_active");
+            } else {
+                $(element).removeClass("swich_btn_item_active")
+                $('#' + $(element).find('a').attr('title')).hide();
+            }
+        }
     })
 });
 //版本 tab切换事件
 $(function () {
     $('#version_tabs a').click(function (e) {
         e.preventDefault();
-        $('#version_tabs li').removeClass("swich_btn_item_active");
-        $("#version_content div").removeClass("version_show");
-        $(this).parent().addClass("swich_btn_item_active");
-        $('#' + $(this).attr('title')).addClass('version_show');
+        var array = $("#version_tabs .swich_btn_item") // 主元素
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            if ($(this).parent().attr('data-index') == index) {
+                $('#' + $(this).attr('title')).show(1000);
+                $(this).parent().addClass("swich_btn_item_active");
+            } else {
+                $(element).removeClass("swich_btn_item_active")
+                $('#' + $(element).find('a').attr('title')).hide();
+            }
+        }
+
+        // $('#version_tabs li').removeClass("swich_btn_item_active");
+        // $("#version_content div").removeClass("version_show");
+        // $(this).parent().addClass("swich_btn_item_active");
+        // $('#' + $(this).attr('title')).addClass('version_show');
     })
 });
 // 客服弹窗
